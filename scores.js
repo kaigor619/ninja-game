136bfa4c2e5a0439981789cf436c9f399ab73669
kaigor619 <kaigor619@gmail.com>
2023-04-24 14:31:21 +0300

    update server url

--- scores.js
+++ scores.js
@@ -4,7 +4,7 @@ window.MicroGame = {
 
     const token = searchParams.get("token");
 
-    return fetch("http://localhost:3000/score", {
+    return fetch("https://ninja-pb6x.onrender.com/score", {
       method: "GET",
       headers: {
         "Access-Control-Allow-Origin": "*",
@@ -17,7 +17,7 @@ window.MicroGame = {
 
     const token = searchParams.get("token");
 
-    return fetch("http://localhost:3000/score", {
+    return fetch("https://ninja-pb6x.onrender.com/score", {
       method: "POST",
       body: JSON.stringify({ score }),
       headers: { "Content-Type": "application/json; charset=UTF-8", token },
