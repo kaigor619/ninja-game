de8c4307cef41c09876ef41b0961820cadc9fde7
kaigor619 <kaigor619@gmail.com>
2023-04-26 13:46:27 +0300

    check server

--- scores.js
+++ scores.js
@@ -1,27 +1,39 @@
+const isLocalhost = true;
+
 window.MicroGame = {
   getHighScore: () => {
     const searchParams = new URLSearchParams(window.location.search);
 
     const token = searchParams.get("token");
 
-    return fetch("https://ninja-pb6x.onrender.com/score", {
-      method: "GET",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        token,
-      },
-    });
+    return fetch(
+      isLocalhost
+        ? "http://localhost:3000/score"
+        : "https://ninja-pb6x.onrender.com/score",
+      {
+        method: "GET",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          token,
+        },
+      }
+    );
   },
   setHighScore: (score) => {
     const searchParams = new URLSearchParams(window.location.search);
 
     const token = searchParams.get("token");
 
-    return fetch("https://ninja-pb6x.onrender.com/score", {
-      method: "POST",
-      body: JSON.stringify({ score }),
-      headers: { "Content-Type": "application/json; charset=UTF-8", token },
-    });
+    return fetch(
+      isLocalhost
+        ? "http://localhost:3000/score"
+        : "https://ninja-pb6x.onrender.com/score",
+      {
+        method: "POST",
+        body: JSON.stringify({ score }),
+        headers: { "Content-Type": "application/json; charset=UTF-8", token },
+      }
+    );
   },
   shareScore: function () {
     if (!window.TelegramGameProxy) {
